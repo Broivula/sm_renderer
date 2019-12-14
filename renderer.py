01d6066526a22a4d936c1f9170a3a84191489ccb
Broivula <elias.koivula1@hotmail.com>
2019-12-15 01:20:26 +0200

    Cleaned up version of the client listener
    TODO: threading, queuing and the rendering

--- renderer.py
+++ renderer.py
@@ -1,11 +1,4 @@
-import socket, ssl
-import requests
-import time
-import logging
 from socketIO_client import SocketIO, LoggingNamespace
-
-HOST = "127.0.0.1"
-PORT = 3000
 
 def on_connect():
 	print('connected!')
@@ -30,25 +23,5 @@ except Exception as err:
 	print('connection failed')
 	print(str(err))
 
-#r = requests.get("https://127.0.0.1:3000", "test", verify="./ssl_certs/ca.crt")
 
-'''
-context = ssl.create_default_context()
-context.check_hostname = False
-context.verify_mode = False
-context.load_verify_locations("./ssl_certs/ca.pem")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s_sock = context.wrap_socket(s)
-s_sock.connect((HOST, PORT))
-time.sleep(2)
-s_sock.send(b"hello");
-data = s_sock.recv(2048)
-
-while True:
-	
-	if len(data) < 1 :
-		break
-	else:
-		print(repr(data))
-'''
 
